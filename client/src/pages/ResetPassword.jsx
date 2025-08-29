03272e22123eea1183525c3bbf8aecc6703fbfee
Harshith917 <147698683+Harshith917@users.noreply.github.com>
2025-08-29 12:50:15 +0530

    fix: removed unused lodash import

--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router';
 import { AppContext } from '../context/AppContext';
 import axios from 'axios';
 import { toast } from 'react-toastify';
-import { set } from 'lodash';
+// import { set } from 'lodash';
 
 const ResetPassword = () => {
 
